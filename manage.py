d7628826352f5fa37a678a7b4ec034ce4d347f27
Raymund Martinez <zhaqenl@gmail.com>
2018-08-24 10:31:25 +0800

    Push first djangogirls commit

---
+++ manage.py
@@ -0,0 +1,22 @@
+#!/nix/store/6y8sfk64x4p926y1sjs89rq3vqjsvd4s-python-2.7.15/bin/python
+import os
+import sys
+
+if __name__ == "__main__":
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
+    try:
+        from django.core.management import execute_from_command_line
+    except ImportError:
+        # The above import may fail for some other reason. Ensure that the
+        # issue is really that Django is missing to avoid masking other
+        # exceptions on Python 2.
+        try:
+            import django
+        except ImportError:
+            raise ImportError(
+                "Couldn't import Django. Are you sure it's installed and "
+                "available on your PYTHONPATH environment variable? Did you "
+                "forget to activate a virtual environment?"
+            )
+        raise
+    execute_from_command_line(sys.argv)
